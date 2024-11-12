7ce119bc640b697e0cff790e0fce43bf78818920
Anass <anassairad@hotmail.com>
2024-11-12 23:12:44 +0100

    exercise 1 is done

--- assignment_1.py
+++ assignment_1.py
@@ -6,28 +6,30 @@ def annual_contribution(income_y0, income_growth, annual_spending, year):
 
 
 # Asking for input on the needed arguments for the parameters set
-year = 0
-age = 20
-initial_investment = 25000 
-end_capital = 0
-cum_deposits_start = 0
-roc_before_tax = 0
-wealth_taxpa = 0
-roc_after_tax = 0
-income = 60000
-income_growth = 0.01
-annual_spending = 45000
-stock_alloc = 0.8
-stock_return = 0.081
-bond_alloc = 0.18
-bond_return = 0.024
-cash_alloc = 0.02
-cash_return = 0
-wealth_taxrate = 0.15
+age = float(input("How old are you?: "))
+income = float(input("What is your annual income today? "))
+initial_investment = float(input("How much dollars would you like to deposit as an initial investment amount?: "))
+income_growth = float(input("With how much percent do you expect your income to grow annually? Write it in decimals: "))
+annual_spending = float(input("How much dollars do you spend annually?: "))
+stock_alloc = float(input("How much percent of your portfolio would you like to invest in stocks? Write it in decimals: "))
+stock_return = float(input("How much percent do you expect the stock return to be? Write it in decimals: "))
+bond_alloc = float(input("How much percent of your portfolio would you like to invest in bonds? Write it in decimals: "))
+bond_return = float(input("How much percent do you expect the bond return to be? Write it in decimals: "))
+cash_alloc = float(input("How much percent of your portfolio would you like to invest in cash? Write it in decimals: "))
+cash_return = float(input("How much percent do you expect the cash return to be? Write it in decimals: "))
+wealth_taxrate = float(input("How much percent is the wealth tax rate?: "))
+retirement_spending_monthly = 2500
+target_withdrawal_rate = 0.04
 
-# Calculating the investment returns, deposits, paid taxed etc.
+# Initialization of variables
+year = 0
+retirement_goal = retirement_spending_monthly * 12 / target_withdrawal_rate
 end_capital = initial_investment
-while year < 2:
+list = []
+temp_list = []
+
+# Calculating the 
+while end_capital < retirement_goal:
     if year == 0:
         cum_deposits_start = initial_investment
     else:
@@ -37,16 +39,21 @@ while year < 2:
     roc_before_tax = \
             (start_capital * stock_alloc * (1 + stock_return) \
             + start_capital * bond_alloc * (1 + bond_return) \
-            + start_capital * cash_alloc * (1 + cash_return)) \
-            - start_capital
+            + start_capital * cash_alloc * (1 + cash_return))
     if roc_before_tax <= 0:
         wealth_taxpaid = 0
     elif roc_before_tax > 0:
         wealth_taxpaid = roc_before_tax * wealth_taxrate
+    roc_before_tax = roc_before_tax - start_capital
     roc_after_tax = roc_before_tax - wealth_taxpaid
     end_capital = roc_after_tax + start_capital
-    print(f"year: {year}, cum_deposits_start: { cum_deposits_start}, start_capital: {start_capital}, cum_returns_start: {cum_returns_start}, roc_before_tax: {roc_before_tax}, wealth_taxpaid: {wealth_taxpaid}, roc_after_tax: {roc_after_tax}, end_capital: {end_capital}")
+    temp_list = [(year + age), annual_contribution(income, income_growth, annual_spending, year), start_capital, cum_deposits_start, roc_before_tax, wealth_taxpaid, roc_after_tax, end_capital]
+    list.append(temp_list.copy())
+    temp_list.clear()
     year += 1
 
-
-# calculating the retirement amount needed
+print(f"You can retire at {int(year + age)}")
+"""
+for item in list:
+    print(item)
+"""
